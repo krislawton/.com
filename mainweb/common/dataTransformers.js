2a2e3d319c42419a5d35f0c81ff8f29f04324514
Kris Lawton <kris.lawton@live.co.uk>
2018-01-27 23:15:29 +0000

    Nomic chat and data transformers - Added time seconds and date long to data transformers  - Added dataTransformers to nomic pages - Nomic messages in chat now only show time instead of full date and time - Date separator cells indicate the date messages were sent. Full date/time is also in the time's hover tooltip.

--- mainweb/common/dataTransformers.js
+++ mainweb/common/dataTransformers.js
@@ -23,6 +23,22 @@ var dataTransformer = (method, input) => {
 		var minute = zeroPad(dateinfo.getMinutes())
 
 		output = dow + " the " + day + " of " + month + ", " + year + " at " + hour + ":" + minute
+	} else if (method === "date long") {
+		var dateinfo = new Date(input)
+		var year = zeroPad(dateinfo.getFullYear()).toString().slice()
+		var month = dateinfo.getMonth() + 1 // 0-11 so + 1
+		if (month === 0) { month = "January" } else if (month === 1) { month = "Febuary" } else if (month === 2) { month = "March" } else if (month === 3) { month = "April" } else if (month === 4) { month = "May" } else if (month === 5) { month = "June" } else if (month === 6) { month = "July" } else if (month === 7) { month = "August" } else if (month === 8) { month = "September" } else if (month === 9) { month = "October" } else if (month === 10) { month = "November" } else if (month === 11) { month = "Deceember" }
+		var day = dateinfo.getDate()
+		if (day === 1) { day += "st" } else if (day === 2) { day += "nd" } else if (day === 3) { day += "rd" } else { day += "th" }
+		var dow = dateinfo.getDay()
+		if (dow === 0) { dow = "Sunday" } else if (dow === 1) { dow = "Monday" } else if (dow === 2) { dow = "Tuesday" } else if (dow === 3) { dow = "Wednesday" } else if (dow === 4) { dow = "Thursday" } else if (dow === 5) { dow = "Friday" } else if (dow === 6) { dow = "Saturday" }
+
+		output = dow + " the " + day + " of " + month + ", " + year
+	} else if (method === "time seconds") {
+		var dateinfo = new Date(input)
+		output = zeroPad(dateinfo.getHours())
+		output += ':' + zeroPad(dateinfo.getMinutes())
+		output += ':' + zeroPad(dateinfo.getSeconds())
 	}
 
 	return output
--- mainweb/views/nomic/chat.js
+++ mainweb/views/nomic/chat.js
@@ -33,18 +33,51 @@
 	// Helper for adding message to board
 	function addMessage(dbRecord, toTop) {
 		var nameColor = (dbRecord.PlayerId !== null ? randomColor(dbRecord.PlayerId) : "hsl(0,0%,0%)")
-		var toAdd = '<tr class="achat" data-messageid="' + dbRecord.MessageId + '">'
-		toAdd += '<td class="timestamp">' + dbRecord.SentDate + '</div>'
+		var toAdd = '<tr class="achat" data-messageid="' + dbRecord.MessageId + '" data-timestamp="' + dbRecord.SentDate + '">'
+
+		var dateString = dataTransformer("time seconds", dbRecord.SentDate)
+		var dateTitle = dataTransformer("datetime long", dbRecord.SentDate)
+
+		toAdd += '<td class="timestamp" title="' + dateTitle + '">' + dateString + '</div>'
 		toAdd += '<td class="else">'
 		toAdd += '<div class="from" style="color: '+nameColor+'">' + dbRecord.PlayerName + '</div>'
 		toAdd += '<div class="contents"></div>'
 		toAdd += '</td></tr>'
 		if (toTop) {
+			// Compare dates
+			var compStampFull = $('#chatContainer .achat:first-of-type').attr('data-timestamp')
+			var compStamp = (typeof compStampFull !== "undefined" ? compStampFull : dbRecord.SentDate).slice(0, 10)
+			var curStamp = (dbRecord.SentDate).slice(0, 10)
+			console.log("Compare stamps " + compStamp + " and " + curStamp)
+			if (compStamp !== curStamp) {
+				var ds = dateSeparator(compStampFull)
+				toAdd += ds
+			}
+
+			// Actually add
 			$('#chatContainer').prepend(toAdd)
 		} else {
+			// Compare dates
+			var prevStampFull = $('#chatContainer .achat:last-of-type').attr('data-timestamp')
+			var prevStamp = (typeof prevStampFull !== "undefined" ? prevStampFull : dbRecord.SentDate).slice(0, 10)
+			var curStamp = (dbRecord.SentDate).slice(0, 10)
+			console.log("COmpare " + prevStamp + " and " + currStamp)
+			if (prevStamp !== curStamp) {
+				var ds = dateSeparator(dbRecord.SentDate)
+				toAdd = ds + toAdd
+			}
+
+			// Actually append
 			$('#chatContainer').append(toAdd)
 		}
 		$('[data-messageid="' + dbRecord.MessageId + '"] .contents').text(dbRecord.Contents)
+	}
+	// Helper for date separators in chat
+	function dateSeparator(dateIn) {
+		var dateOut = new Date(dateIn)
+		dateOut = dataTransformer("date long", dateOut)
+		var ret = '<tr class="dateSeparator" data-timestamp="' + dateIn + '"><td colspan="2">' + dateOut + '</td></tr>'
+		return ret
 	}
 
 	// On message received
@@ -85,9 +118,9 @@
 			unicodeCombined += char.charCodeAt(0)
 		}
 		
-		var randHue = Math.pow(unicodeCombined, 0.755)
+		var randHue = Math.pow(unicodeCombined, 0.755) // default 0.755
 		randHue = Math.round((randHue % 1) * 10000) % 360
-		var randLig = Math.pow(unicodeCombined, 0.432)
+		var randLig = Math.pow(unicodeCombined, 0.432) // default 0.432
 		randLig = Math.round((randLig % 1) * 10000)
 
 		if (randHue >= 40 && randHue <= 200) {
